7f93a162c192f0adee5ccad2dd2cfe573c428969
michael.suzuki@aol.com <michael.suzuki@aol.com>
2021-12-21 13:52:13 +0000

    languageカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_035202) do
+ActiveRecord::Schema.define(version: 2021_12_21_134950) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "admin_name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_12_19_035202) do
     t.string "video"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "language"
   end
 
   create_table "titles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
